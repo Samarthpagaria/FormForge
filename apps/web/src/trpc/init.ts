f1a61834271411289cf2e5261ad1d33975a4a72e
Samarth Pagaria <pagariasamarth@gmail.com>
2026-06-13 18:41:30 +0530

    feat_backend : Clerk integrated

--- apps/web/src/trpc/init.ts
+++ apps/web/src/trpc/init.ts
@@ -1,8 +1,11 @@
-import { initTRPC } from "@trpc/server";
-import {db} from "@formforge/db"
+import { TRPCError, initTRPC } from "@trpc/server";
+import { db } from "@formforge/db"
+import { auth } from "@clerk/nextjs/server";
 
 export const createTRPCContext = async (opts: { headers: Headers }) => {
-  return {db, userId: "user_123" }; // replace with real auth later
+  // fetch the clerk session/auth data from the incoming request headers/cookies
+  const authObject = await auth();
+  return { auth : authObject,db}
 };
 
 const t = initTRPC
@@ -12,3 +15,19 @@ const t = initTRPC
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+
+const isAuthed = t.middleware(({ next, ctx }) => {
+  if (!ctx.auth?.userId) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+return next({
+    ctx: {
+      // Infers that auth and userId are definitely present in protectedProcedures
+      auth: {
+        ...ctx.auth,
+        userId: ctx.auth.userId,
+      },
+    },
+  });
+})
+export const protectedProcedure = t.procedure.use(isAuthed)
