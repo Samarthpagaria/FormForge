816a7e9fda653ba166a2eee46eba424f0c34c6b3
Samarth Pagaria <pagariasamarth@gmail.com>
2026-06-13 16:10:12 +0530

    feat_db : drizzle setup

---
+++ apps/web/src/trpc/routers/templates.ts
@@ -0,0 +1,3 @@
+import { createTRPCRouter } from "../init";
+
+export const templatesRouter = createTRPCRouter({});
--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,6 +1,9 @@
 // drizzle.config.ts — tells Drizzle where your schema is and where to put migrations
 import type {Config} from "drizzle-kit";
-
+import * as dotenv from "dotenv";
+dotenv.config({
+  path: "../../.env",
+});
 export default {
     schema: "./src/schema/index.ts",
     out: "./migrations",
